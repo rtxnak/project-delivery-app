00c5d5337148663eb403569e12ae5e73d73aa8dc
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-07-12 22:26:14 -0300

    feat: adicionou service para ENDPOINT POST no path /login

---
+++ back-end/src/api/services/loginService.js
@@ -0,0 +1,28 @@
+const md5 = require('md5');
+const { Users } = require('../../database/models');
+const { tokenGeneratorCreate } = require('../utils/tokenGenerator');
+
+const login = async (email, password) => {
+  const user = await Users.findOne({ where: { email } });
+
+  if (user === null) {
+    return {
+      code: 404,
+      message: 'invalid fields',
+    };
+  }
+
+  if (user.password !== md5(password)) {
+    return {
+      code: 404,
+      message: 'invalid fields',
+    };
+  }
+  const { id, name, role } = user;
+  const token = tokenGeneratorCreate(role);
+  return { code: 200, content: { id, name, email, role, token } };
+};
+
+module.exports = {
+  login,
+};
