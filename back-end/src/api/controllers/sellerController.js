ea723f5edf6ccce2e70983c17b9651f81d78bfe8
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-07-13 19:17:16 -0300

    feat: adicionou controller para ENDPOINT POST no path /seller/orders

---
+++ back-end/src/api/controllers/sellerController.js
@@ -0,0 +1,20 @@
+const sellerService = require('../services/sellerService');
+
+const getAllSales = async (req, res) => {
+  try {
+    const { sellerId } = req.body;
+    const result = await sellerService.getAll(sellerId);
+    if (result.code === 200) {
+      const { code, content } = result;
+      return res.status(code).json(content);
+    }
+    const { code, message } = result;
+    return res.status(code).json({ message });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+module.exports = {
+  getAllSales,
+};
