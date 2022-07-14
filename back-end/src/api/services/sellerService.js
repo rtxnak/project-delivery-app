897f98f52d43179833fe1dffae3cd6ce91ffffee
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-07-14 16:24:29 -0300

    feat: adicionou service para ENDPOINT PUT no path /seller/orders

--- back-end/src/api/services/sellerService.js
+++ back-end/src/api/services/sellerService.js
@@ -11,6 +11,21 @@ const getAll = async (sellerId) => {
   return { code: 200, content: { sales } };
 };
 
+const update = async (orderId, status) => {
+  const saleUpdated = await Sales.update(
+    { status },
+    { where: { id: orderId } },
+  );
+  const resultCode = saleUpdated[0];
+  if (resultCode === 1) {
+    return { code: 200, message: 'order status updated' };
+  }
+  if (resultCode === 0) {
+    return { code: 400, message: 'order status did not updated' };
+  }
+};
+
 module.exports = {
   getAll,
+  update,
 };
